1e60a202549b6ea2916b8890d05d1c43a4b5ad0d
vincentiusantu <vincentiusantu@gmail.com>
2023-03-24 23:03:22 +0700

    fixing delete caas bug

--- resources/views/delcaas.blade.php
+++ resources/views/delcaas.blade.php
@@ -13,29 +13,32 @@
 <body class="bg-dark-space bg-cover  h-screen w-screen flex flex-col items-center">
     @include('partials.sidebarAdm')
         <h1 class="font-arcade text-white text-3xl text-center py-4">DATA CAAS</h1>
-        <div class="text-dark bg-gray-700 text-center font-pixel text-2xl p-20 rounded-lg">
-            <p class="px-10 text-white">Nama</p>
-            <img class="w-96" src="{{ asset('/assets/dasboardcaas/Underline_Dashed.png') }}" alt="" />
-            <p class="px-10 mb-5">{{ $name }}</p>
-            <p class="px-10 text-white">NIM</p>
-            <img class="w-96" src="{{ asset('/assets/dasboardcaas/Underline_Dashed.png') }}" alt="" />
-            <p class="px-10 mb-5">{{ $nim }}</p>
-            <p class="px-10 text-white">Status</p>
-            <img class="w-96" src="{{ asset('/assets/dasboardcaas/Underline_Dashed.png') }}" alt="" />
-                @if($isPass==1)
-                <p class="text-green-700 px-10 mb-5">Lolos</p>
-                @else
-                <p class="text-red-700 px-10 mb-5">Tidak lolos</p>
-                @endif
-                <p class="px-10 text-white">Tahap</p>
+        <form action="/delcaas/{{$datacaas_id}}" method="post">
+            @csrf
+            <div class="text-dark bg-gray-700 text-center font-pixel text-2xl p-20 rounded-lg">
+                <p class="px-10 text-white">Nama</p>
                 <img class="w-96" src="{{ asset('/assets/dasboardcaas/Underline_Dashed.png') }}" alt="" />
-                <p class="px-10">{{ $stagesname }}</p>
+                <p class="px-10 mb-5">{{ $name }}</p>
+                <p class="px-10 text-white">NIM</p>
+                <img class="w-96" src="{{ asset('/assets/dasboardcaas/Underline_Dashed.png') }}" alt="" />
+                <p class="px-10 mb-5">{{ $nim }}</p>
+                <p class="px-10 text-white">Status</p>
+                <img class="w-96" src="{{ asset('/assets/dasboardcaas/Underline_Dashed.png') }}" alt="" />
+                    @if($isPass==1)
+                    <p class="text-green-700 px-10 mb-5">Lolos</p>
+                    @else
+                    <p class="text-red-700 px-10 mb-5">Tidak lolos</p>
+                    @endif
+                    <p class="px-10 text-white">Tahap</p>
+                    <img class="w-96" src="{{ asset('/assets/dasboardcaas/Underline_Dashed.png') }}" alt="" />
+                    <p class="px-10">{{ $stagesname }}</p>
+                </div>
+            <div class="grid justify-items-center my-4">
+                <button class="w-48 h-14 bg-[#00172B] font-arcade text-center text-white shadow-[inset_0px_0px_8px_2px_#FFFFFF] rounded-[8px] duration-200 hover:shadow-[inset_0px_0px_8px_2px_#f3964a] active:shadow-[inset_0px_0px_8px_2px_#ffd0a9]" type="submit" id="tambahCaas-popup">
+                    <h1 class="duration-200 buttonText flex justify-center items-center h-[48px] hover:text-lg active:text-base" >DELETE AKUN CAAS</h1>
+                </button>
             </div>
-        <div class="grid justify-items-center my-4">
-            <button class="w-48 h-14 bg-[#00172B] font-arcade text-center text-white shadow-[inset_0px_0px_8px_2px_#FFFFFF] rounded-[8px] duration-200 hover:shadow-[inset_0px_0px_8px_2px_#f3964a] active:shadow-[inset_0px_0px_8px_2px_#ffd0a9]" id="tambahCaas-popup">
-                <h1 class="duration-200 buttonText flex justify-center items-center h-[48px] hover:text-lg active:text-base" >DELETE AKUN CAAS</h1>
-            </button>
-        </div>
+        </form>
 
 
         <script src="{{ asset('/js/dataCaas_popup.js') }}"></script>
